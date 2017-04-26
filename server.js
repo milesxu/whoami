54ff73cc7cf34aeacbde5cc9043a6cfb9a278884
Ranger Alone <Ranger Alone>
2017-04-26 18:12:04 +0800

    mission complete

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 "use restrict";
+let express = require("express");
+let app = express();
+
+app.get('/api/whoami', (req, res) => {
+    let lang = req.get('Accept-Language').split(',')[0];
+    let agent = req.get('User-Agent');
+    let [i, j] = [agent.indexOf('('), agent.indexOf(')')];
+    let result = {
+        ipaddress: req.ip,
+        language: lang,
+        software: agent.slice(i + 1, j)
+    };
+    res.json(result);
+});
+
+app.listen(process.env.PORT || 8080, () => console.log("app is running!"));
